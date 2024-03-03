7eb5351050809382b6dedff066a086656b415f75
yinjieshen <yinjieshen411@gmail.com>
2024-03-03 13:21:24 +0800

    Implement SelectSort

--- SortBasic/Sort.h
+++ SortBasic/Sort.h
@@ -3,58 +3,38 @@
 class Sort
 {
 private:
-	Sort();
+    Sort();
 public:
-	template<typename T>
-	static std::vector<T> SelectSort(const std::vector<T>& unordered_array,bool descending = false) {
-		std::vector<T> res;
-		int tmp_index{ 0 }, last_index{ 0 };
-		int length = unordered_array.size();
-		if (descending) {
-			for (int i = 0; i < unordered_array.size(); i++) {
-				for (int j = 0; j < unordered_array.size(); j++) {
-					if (i > 0) {
-						if (unordered_array[j] >= unordered_array[tmp_index] &&
-							unordered_array[j] <= unordered_array[last_index] &&
-							j != last_index) {
-							tmp_index = j;
-						}
-					}
-					else {
-						if (unordered_array[j] >= unordered_array[tmp_index]) {
-							tmp_index = j;
-						}
-					}
-				}
-				res.push_back(unordered_array[tmp_index]);
-				last_index = tmp_index;
-			}
-		}
-		else {
-			for (int i = 0; i < unordered_array.size(); i++) {
-				tmp_index = 0;
-				for (int j = 0; j < unordered_array.size(); j++) {
-					if (i > 0) {
-						if (unordered_array[j] <= unordered_array[tmp_index] &&
-							unordered_array[j] >= unordered_array[last_index] &&
-							j != last_index) {
-							tmp_index = j;
-						}
-					}
-					else {
-						if (unordered_array[j] <= unordered_array[tmp_index]) {
-							tmp_index = j;
-						}
-					}
-				}
-				res.push_back(unordered_array[tmp_index]);
-				last_index = tmp_index;
-			}
-		}
-		return res;
-	}
+    template<typename T>
+    static std::vector<T> SelectSort(const std::vector<T>& unordered_array,bool descending = false) {
+        std::vector<T> ordered_array(unordered_array);
+        InplaceSelectSort(ordered_array, descending);
+        return ordered_array;
+    }
 
-	template<typename T>
-	static void SelectSortInPlace(std::vector<T>&);
+    template<typename T>
+    static void InplaceSelectSort(std::vector<T>& unordered_array, bool descending = false) {
+        int len = unordered_array.size(), tmp{ 0 };
+        if (len < 2)
+            return;
+        for (int i = 0; i < len; i++) {
+            for (int j = i; j < len; j++) {
+                if (descending) {
+                    if (unordered_array[j] > unordered_array[i]) {
+                        tmp = unordered_array[i];
+                        unordered_array[i] = unordered_array[j];
+                        unordered_array[j] = tmp;
+                    }
+                }
+                else {
+                    if (unordered_array[j] < unordered_array[i]) {
+                        tmp = unordered_array[i];
+                        unordered_array[i] = unordered_array[j];
+                        unordered_array[j] = tmp;
+                    }
+                }
+            }
+        }
+    }
 };
 
--- SortBasic/main.cpp
+++ SortBasic/main.cpp
@@ -4,14 +4,25 @@
 #include "Sort.h"
 
 int main() {
+	//1.Generate random array
 	std::vector<int>  rand_array;
-	ArrayGenerator::getArray(rand_array, 10);
+	ArrayGenerator::getArray(rand_array, 20);
+	std::cout << "Original array: \n";
 	for (int i = 0; i < rand_array.size(); i++) {
 		std::cout << rand_array[i] << " \n"[i == rand_array.size()-1];
 	}
-	std::vector<int>  ordered_array = Sort::SelectSort(rand_array);
-	for (int i = 0; i < ordered_array.size(); i++) {
-		std::cout << ordered_array[i] << " \n"[i == ordered_array.size()-1];
+
+	//2.Sorting
+	//Non-inplace Select sort
+	//std::vector<int>  ordered_array = Sort::SelectSort(rand_array);
+	//Inplace Select sort
+	Sort::InplaceSelectSort(rand_array,true);
+
+
+	//3.Output
+	std::cout << "Ordered array: \n";
+	for (int i = 0; i < rand_array.size(); i++) {
+		std::cout << rand_array[i] << " \n"[i == rand_array.size()-1];
 	}
 	return 0;
 }
